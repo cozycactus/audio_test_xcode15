fa7f7a84c26fabffe8bf3567abe5343f6e29d6eb
Ruslan Migirov <trapi78@gmail.com>
2024-09-21 13:54:26 +0300

    Initial Commit

---
+++ audio_test_xcode15/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  audio_test_xcode15
+//
+//  Created by cozy on 21/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
